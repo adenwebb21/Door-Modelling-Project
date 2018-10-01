f95b5acdbbe6ca2bb7e4932e016a32e78e168fbf
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2018-10-01 16:02:11 +0100

    added basic proximity door opening

---
+++ Assets/CheckForPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckForPlayer : MonoBehaviour {
+
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetBool("playerInRange", true);
+        animator.SetTrigger("playerInRANGE");
+    }
+}
---
+++ Assets/InteractingWithDoor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractingWithDoor : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Door")
+        {
+
+        }
+    }
+}
