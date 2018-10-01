4373e22f6d7680731f513529f670f080e57f8627
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2018-10-01 14:35:48 +0100

    Initial commit

---
+++ Assets/Scripts/opendoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class opendoor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Animation>().Play("dooropen");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
