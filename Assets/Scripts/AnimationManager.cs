c03a28fce842e4a7ff6f753ecab9704d88fab1b2
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2018-10-30 12:59:48 +0000

    started on adding amterials and organised files a bit

--- Assets/Scripts/AnimationManager.cs
+++ Assets/Scripts/AnimationManager.cs
@@ -5,25 +5,38 @@ using UnityEngine;
 public class AnimationManager : MonoBehaviour {
 
     public Animator bolt, latch, padlock, bar, door;
+    private bool padlockDone = false;
+    private int locksDone = 0;
 
     public void TriggerAnimation(string name)
     {
         switch(name)
         {
             case "latch":
-                latch.SetTrigger(name);
+                if(padlockDone)
+                {
+                    latch.SetTrigger(name);
+                    locksDone++;
+                }               
                 break;
             case "bolt":
+                locksDone++;
                 bolt.SetTrigger(name);
                 break;
             case "padlock":
+                locksDone++;
+                padlockDone = true;
                 padlock.SetTrigger(name);
                 break;
             case "bar":
+                locksDone++;
                 bar.SetTrigger(name);
                 break;
             case "door":
-                door.SetTrigger(name);
+                if(locksDone == 4)
+                {
+                    door.SetTrigger(name);
+                }               
                 break;
 
         }
---
+++ Assets/Scripts/RemoveItems.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveItems : MonoBehaviour
+{
+    private Transform itemTransform;
+    private float startingZ;
+
+    //private float minDissolve = 0;
+    //private float maxDissolve = 1;
+
+    //private float t = 0.0f;
+
+    //private Renderer rend;
+    //private float dissolveValue;
+
+    private void Start()
+    {
+        itemTransform = transform;
+        startingZ = itemTransform.position.z;
+        //rend = gameObject.GetComponent<Renderer>();
+        //dissolveValue = rend.material.GetFloat("Amount");
+    }
+
+    void Update()
+    {
+        if (itemTransform.position.z - startingZ >= 0.2)
+        {
+            //rend.material.SetFloat("Amount", 0.5f);
+            gameObject.SetActive(false);
+        }
+
+        //rend.material.SetFloat("Amount", Mathf.Lerp(minDissolve, maxDissolve, t));
+        //t += 0.5f * Time.deltaTime;
+    }
+}
