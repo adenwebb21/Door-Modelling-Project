032e472429a891e4512cb04618f038d1eb2277b1
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2018-11-01 13:21:59 +0000

    added flicker script

--- Assets/flicker.cs
+++ Assets/flicker.cs
@@ -4,6 +4,31 @@ using UnityEngine;
 
 public class flicker : MonoBehaviour
 {
+    private Light light;
 
-    
+    private float lightInterval;
+    private float flickerAmount;
+    private float flickerStartTime;
+
+
+    private void Start()
+    {
+        light = gameObject.GetComponent<Light>();
+
+        flickerStartTime = Time.time;
+        
+        flickerAmount = Random.Range(1.1f, 1.7f);
+        lightInterval = Random.Range(0.01f, 0.2f);
+    }
+
+    private void Update()
+    {
+        if(Time.time - flickerStartTime >= lightInterval)
+        {
+            light.intensity = Mathf.Lerp(light.intensity, flickerAmount, 0.5f);
+            flickerAmount = Random.Range(1.1f, 1.7f);
+            lightInterval = Random.Range(0.01f, 0.2f);
+            flickerStartTime = Time.time;
+        }
+    }
 }
