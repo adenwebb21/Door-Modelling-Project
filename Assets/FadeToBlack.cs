86e4998e72f98b18bef085da413054441dff2436
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2018-11-09 14:12:00 +0000

    Finished!!

---
+++ Assets/FadeToBlack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeToBlack : MonoBehaviour {
+
+    public Image blackScreen;
+
+    private void Start()
+    {
+        blackScreen.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        blackScreen.gameObject.SetActive(true);
+        Invoke("LoadScene", 1f);
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(0);
+        return;
+    }
+}
--- Assets/Scripts/AnimationManager.cs
+++ Assets/Scripts/AnimationManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AnimationManager : MonoBehaviour {
 
     public Animator bolt, latch, padlock, bar, door;
+    public AudioSource doorSound, doorBolt, doorPadlock, doorLatch, latchRattle, doorRattle, doorbar;
     private bool padlockDone = false;
     private int locksDone = 0;
 
@@ -15,28 +16,41 @@ public class AnimationManager : MonoBehaviour {
             case "latch":
                 if(padlockDone)
                 {
+                    doorLatch.PlayDelayed(0.4f);
                     latch.SetTrigger(name);
                     locksDone++;
-                }               
+                }   
+                else
+                {
+                    latchRattle.Play();
+                }
                 break;
             case "bolt":
                 locksDone++;
+                doorBolt.PlayDelayed(0.3f);
                 bolt.SetTrigger(name);
                 break;
             case "padlock":
                 locksDone++;
                 padlockDone = true;
+                doorPadlock.Play();
                 padlock.SetTrigger(name);
                 break;
             case "bar":
                 locksDone++;
+                doorbar.Play();
                 bar.SetTrigger(name);
                 break;
             case "door":
                 if(locksDone == 4)
                 {
                     door.SetTrigger(name);
-                }               
+                    doorSound.Play();
+                }       
+                else
+                {
+                    doorRattle.Play();
+                }
                 break;
 
         }
